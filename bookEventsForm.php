b3271265cb7219d1e6990a741fccf6392fa4c3db
Mark Tilbrook <marktilbrook2906@gmail.com>
2020-01-03 15:22:21 +0400

    Updated this file with some JS
    Added some JS

--- bookEventsForm.php
+++ bookEventsForm.php
@@ -20,7 +20,93 @@ $result = curl_exec($curl);
 curl_close($curl);
 echo $result;
 ?>
+<!--//this works-->
+<?php
+
+
+echo "<form method=\"post\" action=\"logOut.php\" style='position: absolute;
+  top: 8px;
+  right: 16px;
+  font-size: 18px;'>
+    <input type=\"submit\" value=\"logout\">
+       </form>";
+?>
 
 <!-- Here you need to add Javascript or a link to a script (.js file) to process the form as required for the assignment -->
+<!--this script answers question B-->
+<script type="text/javascript">
+    window.addEventListener('load', function () {
+        "use strict";
+
+        const l_Form = document.getElementById("bookingForm");
+
+
+        l_Form.termsChkbx.onchange = function () {
+            if (l_Form.termsChkbx.checked == true) {
+                document.getElementById("termsText").style.fontWeight = "normal";
+                document.getElementById("termsText").style.color = "black";
+                l_Form.submit.disabled = false;
+            }
+            if (l_Form.termsChkbx.checked == false) {
+                document.getElementById("termsText").style.fontWeight = "bold";
+                document.getElementById("termsText").style.color = "red";
+                l_Form.submit.disabled = true;
+            }
+        };
+    });
+</script>
+
+
+<!--this works - this script answers question C and D -->
+<script type="text/javascript">
+    window.addEventListener('load', function () {
+        "use strict";
+        const l_form = document.getElementById("bookingForm");
+        const l_surnameField = document.querySelector("#retCustDetails > input[type=text]:nth-child(2)");
+        const l_submit = document.querySelector("#makeBooking > p:nth-child(6) > input[type=submit]");
+
+        l_form.onchange = calculateTotal;
+        l_submit.onclick = validate;
+
+        let isSomethingChecked = false;
+        let isNameEmpty        = false;
+
+        function calculateTotal()
+        {
+            let total = 0;
+            const eventsForm = l_form.querySelectorAll('div.item');
+            const eventsCount = eventsForm.length;
+
+            for (let i = 0; i < eventsCount; i++)
+            {
+                const event = eventsForm[i];
+                const checkBox = event.querySelector('input[data-price][type=checkbox]');
+
+                if (checkBox.checked === true)
+                {
+                    total += parseFloat(checkBox.dataset.price);
+                    isSomethingChecked = true;
+                }
+            }
+            const radio = l_form.querySelector('input[data-price][type=radio]');
+            if (radio.checked === true)
+            {
+                total += parseFloat(radio.dataset.price);
+            }
+            l_form.total.value = total;
+
+        }
+
+        function validate()
+        {
+            if (l_surnameField.value == "" || l_surnameField.value == null || isSomethingChecked == false)
+            {
+                alert("Missing Field...Please Try Again.");
+                return false;
+            }
+        }
+
+    });
+</script>
 </body>
 </html>
